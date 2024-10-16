6a8c934fc8de3aa38e6aac4c10d63522e8ffb381
hasanulhasan <hasanulkarimh@gmail.com>
2024-10-16 22:55:09 +0600

    add some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleClick() {
+    alert("oiii mama ki obostaaaaa tumar heeeeeeee");
+  }
+  const handleClick2 = () => {
+    alert("oiiii mama ami arfat viayaaaaaaa");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Amar name holo Ariful islam arfat</h2>
+      <button onClick={handleClick}>Click Me pls</button>
+      <button onClick={handleClick2}>Click me 2 pls</button>
+      <button
+        onClick={() => {
+          alert("mama ami to shob kichu nije nije kore pelchi tai na mama");
+        }}
+      >
+        ami nije nije korbo
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
